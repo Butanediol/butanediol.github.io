dd46ed426534fd5493434709e0426e3f36607be4
Butanediol <n872485238@gmail.com>
2020-12-29 19:10:10 +0800

    Site updated: 2020-12-29 19:10:09

--- js/navbar_text.js
+++ js/navbar_text.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+	$(".navbar").css("box-shadow" , "0px 0px 0px black");
+	$(".navbar").css("-webkit-box-shadow" , "0px 0px 0px black");
+
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 50) {
+	  	$(".navbar").css("box-shadow" , "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12)");
+	  	$(".navbar").css("-webkit-box-shadow" , "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12)");
+
+
 	  	if (localStorage.getItem("Fluid_Color_Scheme") == "dark") {
 	  		$(".navbar").css("--navbar-text-color" , "#ffffff");
 	  		// console.log("Dark");
@@ -12,6 +19,9 @@ $(document).ready(function(){
 	  }
  
 	  else{
+	  	$(".navbar").css("box-shadow" , "0px 0px 0px black");
+	  	$(".navbar").css("-webkit-box-shadow" , "0px 0px 0px black");
+
 	  	if (localStorage.getItem("Fluid_Color_Scheme") == "dark") {
 	  		$(".navbar").css("--navbar-text-color" , "#ffffff");
 	  		// console.log("Dark");
